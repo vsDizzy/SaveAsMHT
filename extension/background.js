c1b818b55125afbbda3e93a1403fb52b160fe3ee
Dizzy <todizzy@gmail.com>
2016-02-22 06:04:08 +0200

    Initial commit. Minimal functionality.

---
+++ extension/background.js
@@ -0,0 +1,27 @@
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId == 'save') {
+    chrome.pageCapture.saveAsMHTML({ tabId: tab.id }, function(blob) {
+      var filename = sanitize(tab.title) + '.MHT';
+      download(blob, filename);
+    });
+  }
+});
+
+chrome.contextMenus.create({
+  contexts: ['all'],
+  id: 'save',
+  title: 'Save As .MHT...'
+});
+
+function sanitize(filename) {
+  return filename.replace(/[<>:"/\\|?*\x00-\x1F]/g, '');
+}
+
+function download(blob, filename) {
+  chrome.downloads.download({
+    conflictAction: 'prompt',
+    filename: filename,
+    saveAs: true,
+    url: URL.createObjectURL(blob)
+  });
+}
