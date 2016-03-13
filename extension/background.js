dcf4c3ddcbebfd718ca75c7883c8269f1cda0fcc
Dizzy <todizzy@gmail.com>
2016-03-13 05:43:03 +0200

    Fixed multiple event registration.

--- extension/background.js
+++ extension/background.js
@@ -1,17 +1,22 @@
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  if (info.menuItemId == 'save') {
-    chrome.pageCapture.saveAsMHTML({ tabId: tab.id }, function(blob) {
-      var filename = sanitize(tab.title) + '.MHT';
-      download(blob, filename);
-    });
-  }
-});
+chrome.runtime.onStartup.addListener(install);
+chrome.runtime.onInstalled.addListener(install);
 
-chrome.contextMenus.create({
-  contexts: ['all'],
-  id: 'save',
-  title: 'Save As .MHT...'
-});
+function install() {
+  chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == 'save') {
+      chrome.pageCapture.saveAsMHTML({ tabId: tab.id }, function(blob) {
+        var filename = sanitize(tab.title) + '.MHT';
+        download(blob, filename);
+      });
+    }
+  });
+
+  chrome.contextMenus.create({
+    contexts: ['all'],
+    id: 'save',
+    title: 'Save As .MHT...'
+  });
+}
 
 function sanitize(filename) {
   return filename.replace(/[<>:"/\\|?*\x00-\x1F]/g, '');
